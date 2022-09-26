760cbdf5498c2bb5a9e7f9a7adf862851048088e
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2022-09-26 08:43:21 -0300

    fix(routes): add 'return' in all routes

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ hello: "world" });
+  return res.status(200).json({ hello: "world" });
 });
 
 app.get("/home", (res, res) => {
-  res.status(200).json({ route: "home" });
+  return res.status(200).json({ route: "home" });
 });
 
 app.get("/posts", (res, res) => {
-  res.status(200).json({ route: "posts" });
+  return res.status(200).json({ route: "posts" });
 });
 
 app.get("/users", (res, res) => {
-  res.status(200).json({ route: "users" });
+  return res.status(200).json({ route: "users" });
 });
 
 app.listen(3000);
